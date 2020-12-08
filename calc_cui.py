ca4d602c55f5bc19de4868bde8ee9fe6c38a80fb
pigmentink <oobanhon@gmail.com>
2020-12-08 10:46:05 +0900

    入力を除く電卓機能の追加

--- calc_cui.py
+++ calc_cui.py
@@ -1,1 +1,15 @@
-print("hello world")
+a = 10
+b = 30
+print("a : " + str(a))
+print("b : " + str(b))
+mode = 1 # 1,+ 2,- 3,* 4,/
+if mode == 1:
+    print("a + b = " + str(a + b))
+elif mode == 2:
+    print("a - b = " + str(a - b))
+elif mode == 3:
+    print("a * b = " + str(a * b))
+elif mode == 4:
+    print("a / b = " + str(a / b))
+else:
+    print("Error: illegal mode number")
