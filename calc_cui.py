185aaa0fef2827ef4a5d50705f26178114622623
pigmentink <oobanhon@gmail.com>
2020-12-08 10:56:00 +0900

    四則計算はできる状態に改良

--- calc_cui.py
+++ calc_cui.py
@@ -1,15 +1,15 @@
-a = 10
-b = 30
-print("a : " + str(a))
-print("b : " + str(b))
-mode = 1 # 1,+ 2,- 3,* 4,/
-if mode == 1:
+a = int(input("a >>"))
+b = int(input("b >>"))
+print("select mode 1...+(Addition) 2...-(Subtraction) 3...*(Multiply) 4.../(division)")
+mode = int(input("mode:")) # 1,+ 2,- 3,* 4,/
+# modeに沿って計算する
+if mode is 1:
     print("a + b = " + str(a + b))
-elif mode == 2:
+elif mode is 2:
     print("a - b = " + str(a - b))
-elif mode == 3:
+elif mode is 3:
     print("a * b = " + str(a * b))
-elif mode == 4:
+elif mode is 4:
     print("a / b = " + str(a / b))
 else:
     print("Error: illegal mode number")
