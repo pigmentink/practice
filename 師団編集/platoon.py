5f76406fe4a142e2181f2f405f43544a0070456a
pigmentink <oobanhon@gmail.com>
2021-12-05 14:16:51 +0900

    師団編成アプリのデータをgitに移行

---
+++ 師団編集/platoon.py
@@ -0,0 +1,21 @@
+import json
+'''
+squad = 0 # 分隊
+platoon = 0 # 小隊
+company = 0 # 中隊
+battalion = 0 # 大隊
+regiment = 0 # 連隊
+division = 0 # 師団
+'''
+
+json_file = open('team1.json','r',encoding="utf-8")
+json_load = json.load(json_file)
+print(json_load)
+# jsonにデータを追加
+
+# jsonに追加する関数
+def insert():
+    
+    return null
+
+# jsonから削除する関数
