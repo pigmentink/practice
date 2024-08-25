d53249a4549ef542fa5c2d2cf033cc5c43befc13
pigmentink <46780957+pigmentink@users.noreply.github.com>
2024-08-25 09:25:09 +0900

    sikiのテーマをアップロード

---
+++ nord.theme/theme.js
@@ -0,0 +1,94 @@
+{
+	"key": "temp1.theme",
+	"name": "nord",
+	"type": "light",
+	"version": "0.25.17",
+	"color": {},
+	"colorSet": {
+		"id": 1,
+		"name": "Light",
+		"description": "標準的なライトテーマ",
+		"type": "light",
+		"contentBackground": "#4c566a",
+		"theme": "#2e3440",
+		"accent4": "#8fbcbb",
+		"accent": "#88c0d0",
+		"baseForeground": "#d8dee9",
+		"myself": "#8fbcbb",
+		"unread": "#ffffff",
+		"contentForeground": "#eceff4",
+		"anchor": "#bf616a",
+		"accent3": "#5e81ac",
+		"archived": "#2e3440",
+		"error": "#bf616a",
+		"imgLink": "#b48ead",
+		"readonly": "#ebcb8b",
+		"accent2": "#81a1c1",
+		"systemAccent": "#d8dee9",
+		"outLink": "#b48ead",
+		"keyword": "#bf616a",
+		"keyword2": "#d08770",
+		"keyword3": "#ebcb8b"
+	},
+	"size": {
+		"resheader": 11,
+		"resbody": 15,
+		"resaa": 10,
+		"resBodyPaddingBottom": 14,
+		"popupBody": 12,
+		"popupHeader": 10,
+		"postheader": 12,
+		"postbody": 14,
+		"resBorderTop": 0,
+		"threadTabWidth": 200,
+		"panel": 14,
+		"appSplitter": 1,
+		"boardTabPadding": 3,
+		"boardTab": 12,
+		"resBorderTB": 0.5,
+		"minimapWidth": 21
+	},
+	"font": {
+		"ui": null,
+		"tab": null,
+		"mono": null,
+		"res": null,
+		"aa": null,
+		"board": null
+	},
+	"value": {
+		"resbody_lineheight": 1.1,
+		"resheader_lineheight": 1.4
+	},
+	"backimage": {
+		"header": "#2e3440",
+		"thread": "#434c5e ",
+		"activities": "#2e3440",
+		"boardTab": "",
+		"threadTab": "#2e3440",
+		"board": "#2e3440 ",
+		"footer": "{\"transparent\":false,\"color\":\"#2e3440\"}",
+		"sidePane": "",
+		"body": "#2e3440",
+		"popupRes": "#4c566a"
+	},
+	"effect": {
+		"threadTextShadowColor": "#070707",
+		"threadTextShadowForeground": "#fcfcfc",
+		"boardTextShadowColor": "#070707",
+		"boardTextShadowForeground": "#fcfcfc",
+		"boardTextShadowOn": false,
+		"threadTextShadowOn": false,
+		"boardTextShadowBlur": 1
+	},
+	"colorScheme": {
+		"id": 1,
+		"enable": false
+	},
+	"options": {
+		"emoji": "color",
+		"threadTabBack": true,
+		"boardTabBack": true,
+		"color_regulate": false
+	}
+}
